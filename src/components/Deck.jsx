27401539d0995bd215229c19c537c1a461d8dffe
Trey Zima <trey.m.zima@gmail.com>
2025-01-30 17:33:04 -0700

    Completed step one. Able to get a deck from the API, draw and display 52 cards, and when no cards remain in the deck an alert pops up.

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -5,12 +5,12 @@ import DrawCard from "./DrawCard";
 function Deck() {
   const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
-  const [deck, setDeck] = useState("");
+  const [deck, setDeck] = useState(null);
 
   useEffect(() => {
     async function getDeck() {
       const res = await axios.get(`${API_BASE_URL}/new/shuffle/`);
-      setDeck(res.data.deck_id.toString());
+      setDeck(res.data.deck_id);
     }
     getDeck();
   }, []);
--- src/components/DrawCard.jsx
+++ src/components/DrawCard.jsx
@@ -2,19 +2,32 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function DrawCard({ api, deck }) {
-  const [img, setImg] = useState("");
+  const [cards, setCards] = useState([]);
 
   async function cardDraw() {
     const res = await axios.get(`${api}/${deck}/draw/`);
 
-    setImg(res.data.cards[0].image);
+    const newCard = {
+      code: res.data.cards[0].code,
+      image: res.data.cards[0].image,
+    };
+
+    setCards((cards) => [...cards, newCard]);
+
+    if (res.data.remaining === 0) {
+      alert("Error: no cards remaining!");
+    }
   }
 
   return (
-    <div>
+    <>
       <button onClick={() => cardDraw()}>GIMME A CARD!</button>
-      <img src={img} />
-    </div>
+      <div>
+        {cards.map(({ code, image }) => (
+          <img key={code} src={image} alt={`card-${code}`} />
+        ))}
+      </div>
+    </>
   );
 }
 
